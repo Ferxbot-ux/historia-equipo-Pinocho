203e769e8aea0b0a857818306a585a15efa0dd8a
MOLINAAAAAAA <2022348074@uteq.edu.mx>
2025-08-13 09:50:26 -0600

    Correción de las tres historias

--- historia.py
+++ historia.py
@@ -1,4 +1,7 @@
+#Parte 1 de la historia. Integrante 1
 print ("Parte 1: Pinocho era un niño de madera que quería ser un niño de verdad.")
+#Parte 2 de la historia. Integrante 2
+print("Parte 2: Un hada mágica le prometió que, si era valiente y honesto, su deseo se haría realidad.")
 
-
+#Parte 3 de la historia. Integrante 3
 print ("Parte 3: Al final, pinocho se convierte en un niño de verdad y vive feliz con su papá.")
